35d5b82b98c69b55c158a3647f95826c9bb46ac8
KuntaPurnama <tanosutanoo@gmail.com>
2025-07-25 17:57:37 +0700

    Add cors config

--- src/main/java/com/app/messenger/config/CorsConfig.java
+++ src/main/java/com/app/messenger/config/CorsConfig.java
@@ -2,6 +2,9 @@ package com.app.messenger.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,5 +21,20 @@ public class CorsConfig {
                         .allowedHeaders("*");
             }
         };
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        // Allow anyone and anything access. Probably ok for Swagger spec
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        source.registerCorsConfiguration("/v2/api-docs", config);
+        return new CorsFilter(source);
     }
 }
